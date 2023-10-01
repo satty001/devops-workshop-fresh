80371f806dc0945520fdb6f14d8b71b52d7f21ba
satty001 <singh_sk@outlook.in>
2023-10-01 17:15:00 +0530

    adding v2 version

--- terraform_code/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-
-resource "aws_instance" "demo-server" {
-    ami = "ami-067c21fb1979f0b27"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-}
---
+++ terraform_code/V2-EC2.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-067c21fb1979f0b27"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+    security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
