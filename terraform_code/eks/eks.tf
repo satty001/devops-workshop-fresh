17f212a1ffd5d3b5c22d772fc499f49a3a083838
satty001 <singh_sk@outlook.in>
2023-10-13 20:52:03 +0530

    updated eks.tf

--- terraform_code/eks/eks.tf
+++ terraform_code/eks/eks.tf
@@ -36,18 +36,18 @@ resource "aws_iam_role" "worker" {
     name = "ed-eks-worker"
 
     assume_role_policy = <<POLICY
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "ec2.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
     POLICY
 }
 
